53a9e9d20453ea4b0198ca413b8b5069a0b30b38
William Carroll <wpcarro@gmail.com>
2020-02-17 16:50:23 +0000

    Add notifications.el as a dependency
    `M-x byte-compile-file` was failing because of the missing dependency on
    notifications.el.
    
    Additionally, notifications.el depends on Emacs being compiled with dbus
    support. Adding a note about that dependency in the Commentary section.

--- egg-timer.el
+++ egg-timer.el
@@ -31,9 +31,17 @@
 
 ;;; Commentary:
 ;; Select common timer intervals and use `notifications-notify' to display a
-;; message when the timer completes.
+;; message when the timer completes.  Since this depends on
+;; `notifications-notify', ensure that your Emacs is compiled with dbus
+;; support.
 
 ;;; Code:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Dependencies
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'notifications)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Library
